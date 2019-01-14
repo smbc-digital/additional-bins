068c04f55bec3b4991cf4189ee04ad39f5d99f0e
Anthony <Anthony-Haslett@outlook.com>
2019-01-14 13:28:30 +0000

    Anthony // Adding: A-Z footer, breadcrumbs - Updating: Waste diary content, now H3

--- src/components/AdditionalBins/6-Success/index.js
+++ src/components/AdditionalBins/6-Success/index.js
@@ -1,31 +1,45 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component} from 'react'
 import PropTypes from 'prop-types'
 import withContext from '../../WithContext'
+import showBreadCrumbs from '../../../helpers/breadcrumbHelper'
+import showAToZFooter from  '../../../helpers/aToZFooterHelper'
 
+export class Success extends Component {
 
-export const Success = ({ context: {crmCaseReference} }) => {
+    constructor(props){
+        super(props)
+        this.props.context
+    }
 
-    return(
-        <Fragment>
-                <section className="header-container">
-                    <h1>Request an additional black bin</h1>
-                    <p className="h2">Thanks for your request</p>
-                    <p className="h3">Case reference: {crmCaseReference} 12345_CHANGE_ME</p>
-                </section>
-                <h2>What happens next</h2>
-                <p>You’ll receive a confirmation email of your request for an additional black bin.</p>
-                <h2>Waste Diary</h2>
-                <p>To help us understand why you need an additional black bin, you must download and fill in a waste diary every day for a week.</p>
-                <p>The email confirmation will tell you how to return your waste diary to us once you have completed it.</p>
-                <a href='https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/pdf/BinCalendars/Waste+diary+v2.pdf'>Download a waste diary</a>
-        </Fragment>
-    )
+    componentDidMount = () => {
+        showBreadCrumbs(true)
+        showAToZFooter(true)
+    }
+
+    render() {
+        const { crmCaseReference } = this.props.context
+
+        return(
+            <Fragment>
+                    <section className="header-container">
+                        <h1>Request an additional black bin</h1>
+                        <p className="h2">Thanks for your request</p>
+                        <p className="h3">Case reference: {crmCaseReference} 12345_CHANGE_ME</p>
+                    </section>
+                    <h2>What happens next</h2>
+                    <p>You’ll receive a confirmation email of your request for an additional black bin.</p>
+                    <h3>Waste diary</h3>
+                    <p>To help us understand why you need an additional black bin, you must download and fill in a waste diary every day for a week.</p>
+                    <p>The email confirmation will tell you how to return your waste diary to us once you have completed it.</p>
+                    <a className='button-primary' href='https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/pdf/BinCalendars/Waste+diary+v2.pdf'>Download a waste diary</a>
+            </Fragment>
+        )
+    }
 }
 
 
 Success.propTypes = {
     context: PropTypes.object,
-    history: PropTypes.object,
 }
 
 
