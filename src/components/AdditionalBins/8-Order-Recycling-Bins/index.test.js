041c8f1e5c610fbf4689de4818c8feeb51af48cd
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-01-14 10:53:51 +0000

    fix(Google Analytics): Add GA page views

--- src/components/AdditionalBins/8-Order-Recycling-Bins/index.test.js
+++ src/components/AdditionalBins/8-Order-Recycling-Bins/index.test.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import ReactGA from 'react-ga'
 import { OrderRecyclingBins } from './index'
 import renderer from 'react-test-renderer'
 
 
 Enzyme.configure({ adapter: new Adapter() })
 describe('OrderRecyclingBins',() => { 
+
+    beforeAll(() => {
+        ReactGA.initialize('testGA', { testMode: true , titleCase: false})
+    })
+
+    beforeEach(() => {
+        ReactGA.testModeAPI.calls.length = 0
+    })
+
+    it('should dispatch google analytics page view', () => {
+        mount(<OrderRecyclingBins context={{}}/>)
+
+        expect(ReactGA.testModeAPI.calls).toHaveLength(1)
+    })
 
     it('should push to correct page', () => {
         // Arrange
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import Provider from './components/Provider'
 import { Router } from 'react-router'
+import ReactGA from 'react-ga'
 import createHistory from 'history/createBrowserHistory'
 import ScrollToTop from './components/ScrollToTop'
 
 let history = createHistory()
+const rootElem = document.getElementById('root')
+
+if (rootElem.dataset.analyticsCode) {
+	ReactGA.initialize(rootElem.dataset.analyticsCode, {titleCase: false})
+}
 
 ReactDOM.render(
 	<Provider>
