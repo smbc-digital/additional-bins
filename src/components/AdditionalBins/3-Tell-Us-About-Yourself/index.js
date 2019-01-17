83d3210d9fbea7544b33b3290a35275f3cfb5600
Anthony <Anthony-Haslett@outlook.com>
2019-01-17 09:29:26 +0000

    Anthony // Removing orange alert, adding PDF info

--- src/components/AdditionalBins/3-Tell-Us-About-Yourself/index.js
+++ src/components/AdditionalBins/3-Tell-Us-About-Yourself/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import withContext from '../../WithContext'
 import { getPageRoute } from '../../../helpers/pagehelper'
-import { AlertForm, TextInputContainer, Button, Anchor, AddressPicker } from 'smbc-react-components'
+import { TextInputContainer, Button, Anchor, AddressPicker } from 'smbc-react-components'
 
 export const TellUsAboutYourself = ({ context: { onChange, address, firstName, lastName, emailAddress, phoneNumber, whyMoreSpace }, history }) => {
     
@@ -21,10 +21,6 @@ export const TellUsAboutYourself = ({ context: { onChange, address, firstName, l
         <Fragment>
             <form onSubmit={onSubmit}>
                 <h1>Request an additional black bin</h1>
-                <AlertForm
-                level='warning'
-                content='You must be a Stockport resident to order an additional black bin.'
-                />
                 <h2>Tell us about yourself</h2>
                 <TextInputContainer onChange={onChange} value={firstName.value} optional={false} maxLength='60' id='firstName' type='text' label='First name' />
                 <TextInputContainer onChange={onChange} value={lastName.value} optional={false} maxLength='60' id='lastName' type='text' label='Last name' />
