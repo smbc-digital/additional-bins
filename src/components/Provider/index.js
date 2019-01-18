2514f0f157064fc1acac1208c30c7cb72412e83c
Anthony <Anthony-Haslett@outlook.com>
2019-01-18 12:00:07 +0000

    Jordan/Anthony - Added mock reference number to showcase success page

--- src/components/Provider/index.js
+++ src/components/Provider/index.js
@@ -41,7 +41,7 @@ class Provider extends Component{
 			},
 			displayRecaptcha: document.getElementById('displayRecaptcha') != null ? document.getElementById('displayRecaptcha').innerHTML === 'true' ? true : false : false,
 			crmReference: '',
-			receiptNumber: '',
+			receiptNumber: '123',
 			binDeliveryDate: '',
 			onChange: this.onChange
 		}
