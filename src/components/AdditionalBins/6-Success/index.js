b2c41b8d37fd0fe6e2c1948ecedaee84933d0c97
Anthony <Anthony-Haslett@outlook.com>
2019-01-18 11:49:40 +0000

    style(SuccessPage): Changed content on both success pages

--- src/components/AdditionalBins/6-Success/index.js
+++ src/components/AdditionalBins/6-Success/index.js
@@ -26,10 +26,11 @@ export class Success extends Component {
                     <h1>{HEADING}</h1>
                     <p className="h2">Thanks for your request</p>
                 </section>
-                <p><strong> Case reference:</strong> {crmCaseReference} 12345_CHANGE_ME</p>
+                { receiptNumber && <p>Thanks for your payment, your request has been approved.</p> }
+                <p><strong>Your reference number is:</strong> {crmCaseReference} 12345_CHANGE_ME</p>
                 { receiptNumber ?
                     <Fragment>
-                        <p><strong>Receipt number:</strong> {receiptNumber} 12345_CHANGE_ME_TOO</p> <p>Thanks for your payment, your request has been approved. You&apos;ll receive a confirmation email</p>
+                        <p><strong>Your receipt number is:</strong> {receiptNumber} 12345_CHANGE_ME_TOO</p> <p>Please keep these numbers safe as you&apos;ll need them if you need to contact us about your bin delivery. You&apos;ll also receive an email with this information.</p>
                         <h2>What happens next</h2>
                         <p>The additional black bin will be delivered on <strong>{ binDeliveryDate } 17/01/19/CHANGEME</strong>. You do not need to be in to accept the bin, we’ll leave it at the front edge of your property.</p>
                         <p>Please note: all bins remain the property of the council.</p>
@@ -37,11 +38,10 @@ export class Success extends Component {
                     </Fragment>
                     : 
                     <Fragment>
+                        <p>Please keep this number safe as you&apos;ll need it for the next step. You&apos;ll also receive an email with this information.</p>
                         <h2>What happens next</h2>
-                        <p>You’ll receive a confirmation email of your request for an additional black bin.</p>
-                        <h3>Waste diary</h3>
-                        <p>To help us understand why you need an additional black bin, you must download and fill in a waste diary every day for a week.</p>
-                        <p>The email confirmation will tell you how to return your waste diary to us once you have completed it.</p>
+                        <p>You must download and fill in a waste diary to help us understand why you need an additional black bin. You&apos;ll need to record the waste you put in your black bin over 7 days.</p>
+                        <p>You can use the button below to download the waste diary. You&apos;ll also receive a link in the email if you want to complete it later. The email will tell you how to return your waste diary to us once you’ve completed it.</p>
                         <a className='button-primary' href='https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/pdf/BinCalendars/Waste+diary+v2.pdf'>Download a waste diary (PDF 65.5Kb)</a>
                     </Fragment>
                 }
