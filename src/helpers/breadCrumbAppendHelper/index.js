82cd6088afc64bd0a15ed7a95cb8bccdd03dc0db
Anthony <Anthony-Haslett@outlook.com>
2019-01-25 14:05:57 +0000

    feat(AdditionalBins): Updating append bread crumbs function + tests &:Simon

--- src/helpers/breadCrumbAppendHelper/index.js
+++ src/helpers/breadCrumbAppendHelper/index.js
@@ -1,8 +1,21 @@
 const appendBreadcrumb = (breadcrumbText, url) => {
     let breadcrumbs = document.getElementsByTagName('ul')    
     if(breadcrumbs.length !== 0){
-        let breadcrumb = breadcrumbs[0]
-        breadcrumb.innerHTML = breadcrumb.innerHTML + '<li><a href="'+ url+ '">'+ breadcrumbText+'</a></li>'
+        let ul = breadcrumbs[0]
+        let li = document.createElement('li')
+        li.innerHTML = '<a href="'+ url+ '">'+ breadcrumbText+'</a>'
+        let breadCrumbListItems = ul.getElementsByTagName('li')
+        let listSize = breadCrumbListItems.length
+        let lastbutone = listSize - 1
+        let arr = []
+        if (breadCrumbListItems.length !== 0){
+           arr = Array.from(breadCrumbListItems)
+           arr.splice(lastbutone, 0, li)           
+        }
+        ul.innerHTML = ''
+        for(let i = 0; i < arr.length; i++){
+            ul.append(arr[i])
+        }
     }
 }
 
--- src/helpers/breadCrumbAppendHelper/index.test.js
+++ src/helpers/breadCrumbAppendHelper/index.test.js
@@ -3,12 +3,12 @@ import appendBreadCrumb from './index.js'
 describe('appendBreadCrumb', () => {
     it('should find ul with class bread crumb and append list item' , () => {
         // Arrange
-        document.body.innerHTML = '<div id="appendBreadCrumb" class="appendBreadCrumb"><ul class="center-wrapper"><li>breadcrumb</li></ul></div>'
+        document.body.innerHTML = '<div id="appendBreadCrumb" class="appendBreadCrumb"><ul class="center-wrapper"><li>breadcrumb</li><li>last bread crumb</li></ul></div>'
 
         // Act
         appendBreadCrumb('text', 'url')
         var result = document.getElementsByTagName('ul')[0].innerHTML        
         // Assert
-        expect(result).toEqual('<li>breadcrumb</li><li><a href="url">text</a></li>')
+        expect(result).toEqual('<li>breadcrumb</li><li><a href="url">text</a></li><li>last bread crumb</li>')
     })    
 })
