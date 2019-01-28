617b4f34c15e9ce2a223592101664d2e0279b865
Anthony <Anthony-Haslett@outlook.com>
2019-01-28 13:27:15 +0000

    content(AdditionalBins): added font awesome exclamation mark to end points

--- src/components/AdditionalBins/7-How-To-Recycle-More/index.js
+++ src/components/AdditionalBins/7-How-To-Recycle-More/index.js
@@ -6,7 +6,6 @@ import { WhatYouCanPutInYourBinsLink } from '../../Config'
 import showBreadCrumbs from '../../../helpers/breadcrumbHelper'
 import showAToZFooter from '../../../helpers/aToZFooterHelper'
 
-
 export class HowToRecycleMore extends Component {
     constructor(props){
         super(props)
@@ -21,12 +20,24 @@ export class HowToRecycleMore extends Component {
     render() {
         return(
             <Fragment>
-                    <h1>We&apos;re sorry but you can&apos;t request an additional black bin</h1>
+                    <div className="ineligible-warning">
+                    <div className="header-container">
+                    
+                    <div>
+                        <i className="fa fa-exclamation" aria-hidden="true"></i>
+                    </div>
+                    
+                    <h1> We&apos;re sorry but you can&apos;t request an additional black bin</h1>
+                    </div>
                     <h2>Why not?</h2>
                     <p>You must recycle as much of your waste as possible.</p>
                     <h3>What you can do next</h3>
                     <p>To find out about what goes in each bin and how you can recycle as much waste as possible, visit our recycling pages using the button below.</p>
                     <a className='button-primary' href={WhatYouCanPutInYourBinsLink} id='what-you-can-put-in-your-bins'>What you can put in your bins</a>
+                    </div>
+                    
+                    
+                    
             </Fragment>
         )
     }
--- src/components/AdditionalBins/8-Order-Recycling-Bins/index.js
+++ src/components/AdditionalBins/8-Order-Recycling-Bins/index.js
@@ -1,42 +1,50 @@
-import React, { Fragment, Component} from 'react'
+import React, { Fragment, Component } from 'react'
 import ReactGA from 'react-ga'
 import { getPageRoute } from '../../../helpers/pagehelper'
 import withContext from '../../WithContext'
 import { OrderRecyclingBinsLink } from '../../Config'
 import showBreadCrumbs from '../../../helpers/breadcrumbHelper'
-import  appendBreadCrumb from '../../../helpers/breadCrumbAppendHelper'
-import showAToZFooter from  '../../../helpers/aToZFooterHelper'
+import appendBreadCrumb from '../../../helpers/breadCrumbAppendHelper'
+import showAToZFooter from '../../../helpers/aToZFooterHelper'
 
-export class  OrderRecyclingBins extends Component{
-    constructor(props) {
-        super(props)    
-        ReactGA.pageview(getPageRoute(8))
-    }
-
-    componentDidMount() {
-        showBreadCrumbs(true)
-        appendBreadCrumb('Request a new bin', 'https://www.stockport.gov.uk/topic/request-a-new-bin')
-        showAToZFooter(true)
+export class OrderRecyclingBins extends Component {
+	constructor(props) {
+		super(props)
+		ReactGA.pageview(getPageRoute(8))
 	}
 
-    render()
-    {
-        return(
-        <Fragment>
-            <form onSubmit={this.onSubmit}>
-                <h1>It looks like you need additional recycling bins</h1>
-                <h2>What you can do next</h2>
-                <p>You can order additional recycling bins so that you</p>
-                <ul>
-                    <li>recycle as much as possible</li>
-                    <li>have more space in your black bin for your non-recyclable waste</li>
-                </ul>
-                <p>Use the button below to order additional recycling bins.</p>
-                <a href={OrderRecyclingBinsLink} className='button-primary'>Order additional recycling bins</a>
-            </form>
-        </Fragment>
-    )
-    }
+	componentDidMount() {
+		showBreadCrumbs(true)
+		appendBreadCrumb('Request a new bin', 'https://www.stockport.gov.uk/topic/request-a-new-bin')
+		showAToZFooter(true)
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<form onSubmit={this.onSubmit}>
+					<div className="ineligible-warning">
+						<div className="header-container">
+							<div>
+								<i className="fa fa-exclamation" aria-hidden="true" />
+							</div>
+							<h1>It looks like you need additional recycling bins</h1>
+						</div>
+						<h2>What you can do next</h2>
+						<p>You can order additional recycling bins so that you</p>
+						<ul>
+							<li>recycle as much as possible</li>
+							<li>have more space in your black bin for your non-recyclable waste</li>
+						</ul>
+						<p>Use the button below to order additional recycling bins.</p>
+						<a href={OrderRecyclingBinsLink} className="button-primary">
+							Order additional recycling bins
+						</a>
+					</div>
+				</form>
+			</Fragment>
+		)
+	}
 }
 
 export default withContext(OrderRecyclingBins)
