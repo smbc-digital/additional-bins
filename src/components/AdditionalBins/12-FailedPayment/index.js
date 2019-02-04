9ff8560fc475a154533511f1a2fdb6335778100e
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-02-04 09:48:33 +0000

    fix(FailedPayment): Add display anchor as a block

--- src/components/AdditionalBins/12-FailedPayment/index.js
+++ src/components/AdditionalBins/12-FailedPayment/index.js
@@ -27,7 +27,7 @@ class FailedPayment extends Component {
             </div>
             <p>Something went wrong. Please try again if you want to continue with the order</p>
             <a className="button-primary" href={parsedQueryString.paymenturl}>Continue with the order</a>
-            <a className="button-secondary" href="https://stockport.gov.uk/">Go to the homepage</a>
+            <a className="button-secondary anchor-as-block" href="https://stockport.gov.uk/">Go to the homepage</a>
         </div>
     }
 }
