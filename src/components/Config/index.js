bcc64a81a01e586f962e7f8281f3ba4430a8ad1a
Jon Chiles <jonchiles@yahoo.com>
2019-03-29 09:35:13 +0000

    Chore(Bin price) updated bin price to £15 and updated the snapshots

--- src/components/Config/index.js
+++ src/components/Config/index.js
@@ -1,4 +1,4 @@
-export const PRICE = '£10'
+export const PRICE = '£15'
 export const HEADING = 'Request an additional black bin'
 export const WhatYouCanPutInYourBinsLink = 'https://www.stockport.gov.uk/what-you-can-put-in-your-bins'
 export const OrderRecyclingBinsLink = 'https://www.stockport.gov.uk/start/additional-recycling-bins'
