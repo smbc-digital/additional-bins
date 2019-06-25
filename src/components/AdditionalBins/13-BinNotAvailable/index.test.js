b7ae1093a039f0d16df130bf7d2c4cdadd59eee2
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-25 13:50:17 +0100

    fix(BinNotAvailable Tests) - removed unused mount import

--- src/components/AdditionalBins/13-BinNotAvailable/index.test.js
+++ src/components/AdditionalBins/13-BinNotAvailable/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme, { mount } from 'enzyme'
+import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import renderer from 'react-test-renderer'
 import BinNotAvailable from './index'
