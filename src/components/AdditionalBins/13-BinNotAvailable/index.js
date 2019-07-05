c25754aae91c5cf384077797c0f276899b808b63
Jordan <jordan.burns@stockport.gov.uk>
2019-07-05 13:47:17 +0100

    Simon/Jordan #709 added text for src/components/AdditionalBins/13-BinNotAvailable/index.js

--- src/components/AdditionalBins/13-BinNotAvailable/index.js
+++ src/components/AdditionalBins/13-BinNotAvailable/index.js
@@ -5,8 +5,20 @@ class BinNotAvailable extends Component {
         super(props)
     }
 
-    render(){
-        return <h1>The bin is not available</h1>
+    render() {
+        return <section>
+            <h1>We’re sorry, but you can’t use this form</h1>
+            <h2>Why not?</h2>
+            <p>You must live in a property that already has a black wheelie bin.</p>
+            <p>To be eligible for an additional black wheelie bin, you must have enough outside space to store it off the road.
+                Properties that are not eligible include; flats above shops or terrace properties without a garden.
+            </p>
+            <h2>What you can do next</h2>
+            <p>If you still think you’re eligible for an additional black wheelie bin or you don’t have enough
+            space for your current non-recyclable waste, please call us on <strong>0161 217 6111</strong> to discuss your needs.
+            </p>
+            <a href="https://www.stockport.gov.uk" className="button-primary">Go to the homepage</a>
+        </section>
     }
 }
 
